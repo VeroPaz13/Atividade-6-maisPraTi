75f5e1384dbebe58f5a0846848cc1708a6f2f440
Verônica Oliveira <veronica_paz13@hotmail.com>
2025-09-14 18:19:38 -0300

    primeiro commit

---
+++ src/data/products.js
@@ -0,0 +1,52 @@
+const products = [
+  {
+    id: 1,
+    title: "Camiseta Básica",
+    price: 49.9,
+    rating: 4,
+    tag: "Novo",
+    image: "camisetaBasica.jpg"
+  },
+  {
+    id: 2,
+    title: "Calça Jeans",
+    price: 129.9,
+    rating: 5,
+    tag: "Promo",
+    image: "jeansMasculino.jpg"
+  },
+  {
+    id: 3,
+    title: "Tênis Heróis",
+    price: 199.9,
+    rating: 4,
+    tag: "Novo",
+    image: "tenisHerois.jpg"
+  },
+  {
+    id: 4,
+    title: "Jaqueta Jeans",
+    price: 159.9,
+    rating: 3,
+    tag: "Promo",
+    image: "jaquetaJeans.jpg"
+  },
+  {
+    id: 5,
+    title: "Bolsa Feminina",
+    price: 89.9,
+    rating: 5,
+    tag: "Novo",
+    image: "bolsaFeminina.jpg"
+  },
+  {
+    id: 6,
+    title: "Relógio Digital Casio",
+    price: 299.9,
+    rating: 4,
+    tag: "Promo",
+    image: "casioRelogio.jpg"
+  }
+];
+
+export default products;
